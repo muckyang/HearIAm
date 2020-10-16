9606f23c524ebb4780205ee38ef92d61dfdc32bc
임도현 <gigjf@naver.com>
2020-10-16 15:30:41 +0900

    login

--- backend/src/main/java/com/ssafy/backend/model/User.java
+++ backend/src/main/java/com/ssafy/backend/model/User.java
@@ -14,10 +14,6 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 @Entity
 @Table(name = "users", uniqueConstraints = { @UniqueConstraint(columnNames = { "id" }),
 		@UniqueConstraint(columnNames = { "name" }) })
