c7e9ed07c1f079c68cdccaffe106d7049773f55a
김영남 <yn782@naver.com>
2020-10-19 17:53:48 +0900

    STT Google python-speech-recognition/ .war O, mp3 X

--- backend/AI/SpeechToText.py
+++ backend/AI/SpeechToText.py
@@ -1,12 +1,28 @@
-import sys
-import io
+import speech_recognition as sr
+sr.__version__
 
 
-def main(argv):
-    print(argv[1])
-    print(sys.modules)
+r = sr.Recognizer()
 
-if __name__ == "__main__":
-    sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
-    sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
-    main(sys.argv)
+harvard = sr.AudioFile('./backend/.mvn/audio/test1.mp3')
+with harvard as source:
+    audio = r.record(source)
+
+r.recognize_google(audio)
+
+with harvard as source:
+    audio1 = r.record(source, duration=4)
+    audio2 = r.record(source, duration=4)
+    audio3 = r.record(source, duration=4)
+    audio4 = r.record(source, duration=4)
+    audio5 = r.record(source, duration=4)
+print(r.recognize_google(audio1))
+print(r.recognize_google(audio2))
+print(r.recognize_google(audio3))
+print(r.recognize_google(audio4))
+print(r.recognize_google(audio5))
+
+
+with harvard as source:
+    audio = r.record(source, offset=7, duration=3)
+    print(r.recognize_google(audio))
