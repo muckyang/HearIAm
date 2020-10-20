d46e479f4091ea4bd63ca144b1c9e3e293decfd2
임도현 <gigjf@naver.com>
2020-10-20 17:57:28 +0900

    send emotion webRTC

---
+++ frontend/src/views/webRTC/UserWRTC.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <WebRTCComp />
+  </div>
+</template>
+
+<script>
+import WebRTCComp from "@/components/webRTC/UWebRTCComp.vue";
+export default {
+  name: "UserWRTC",
+  components: {
+    WebRTCComp,
+  },
+};
+</script>
+
+<style>
+</style>
