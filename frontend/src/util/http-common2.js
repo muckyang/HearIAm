7ec49251f612f10e51613aceedb5ad45b5964438
이재인 <skyfin59@naver.com>
2020-10-22 16:53:54 +0900

    http url modify2;

--- frontend/src/util/http-common2.js
+++ frontend/src/util/http-common2.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
     // baseURL: "http://localhost:8080/api/auth",
-    baseURL: "https://j3b108.p.ssafy.io:8080/api/auth",
+    baseURL: "http://k3b202.p.ssafy.io:8080/api/auth",
     headers: {
         "Content-type": "application/json"
     }
