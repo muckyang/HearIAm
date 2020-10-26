74bd21af658b290fbdf8453aed1b7c8a44764c39
노휘종 <gnlwhd6498@naver.com>
2020-10-26 05:40:45 +0000

    Update backend/AI/ocr.py

--- backend/AI/ocr.py
+++ backend/AI/ocr.py
@@ -13,7 +13,8 @@ def main(argv):
         with open("./frontend/public/images/" + argv[1], "rb") as f:
             img = base64.b64encode(f.read())
     else:
-        with open("/var/lib/jenkins/workspace/front/frontend/public/images/" + argv[1], "rb") as f:
+        # with open("/var/lib/jenkins/workspace/front/frontend/public/images/" + argv[1], "rb") as f:
+        with open("/var/lib/jenkins/workspace/front/frontend/public/images/kisa.jpg", "rb") as f:
             img = base64.b64encode(f.read())
         # r = open('/var/lib/jenkins/workspace/maven-test/frontend/public/textFiles/' + argv[1], mode='rt', encoding='utf-8')
 
