ec44fecb599e7403c725a7298cdb03555610f931
노휘종 <gnlwhd6498@naver.com>
2020-10-26 08:53:29 +0000

    Update backend/src/main/java/com/ssafy/backend/controller/CertificationController.java, frontend/src/util/api.js, frontend/src/util/api2.js, frontend/src/util/http-common.js, frontend/src/util/http-common2.js, frontend/src/util/http-common3.js files

--- backend/src/main/java/com/ssafy/backend/controller/CertificationController.java
+++ backend/src/main/java/com/ssafy/backend/controller/CertificationController.java
@@ -22,7 +22,7 @@ import io.swagger.annotations.ApiOperation;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api/certCK")
+@RequestMapping("/api/cert")
 public class CertificationController {
 
 	// @Autowired
@@ -31,27 +31,26 @@ public class CertificationController {
 	@GetMapping("/imgCheck/{img}")
 	@ApiOperation(value = "자격증 OCR")
 	public Object textCheck(@PathVariable String img) throws SQLException, IOException {
-		// System.out.println("Certification Check Python Call");
-		// String[] command = new String[3];
-		// command[0] = "python";
-		// // 경로 확인
-		// String hostname = InetAddress.getLocalHost().getHostName();
+		System.out.println("Certification Check Python Call");
+		String[] command = new String[3];
+		command[0] = "python";
+		// 경로 확인
+		String hostname = InetAddress.getLocalHost().getHostName();
 
-		// if (hostname.substring(0, 7).equals("DESKTOP")) {// local
-		// 	command[1] = "./backend/AI/ocr.py";
-		// } else {// aws
-		// 	command[1] = "../AI/ocr.py";
-		// }
+		if (hostname.substring(0, 7).equals("DESKTOP")) {// local
+			command[1] = "./backend/AI/ocr.py";
+		} else {// aws
+			command[1] = "../AI/ocr.py";
+		}
 
-		// // 파일 이름
-		// command[2] = img;
+		// 파일 이름
+		command[2] = img;
 
-		return 1;
-		// try {
-		// 	return execPython(command);
-		// } catch (Exception e) {
-		// 	return "fail";
-		// }
+		try {
+			return execPython(command);
+		} catch (Exception e) {
+			return "fail";
+		}
 	}
 
 	public static Object execPython(String[] command) throws IOException, InterruptedException {
--- frontend/src/util/http-common3.js
+++ frontend/src/util/http-common3.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // axios 객체 생성
 export default axios.create({
     // baseURL: "http://localhost:8080/api",
-    baseURL: "http://k3b202.p.ssafy.io:8080/api",
+    baseURL: "https://k3b202.p.ssafy.io:8080/api",
     headers: {
         "Content-type": "multipart/form-data"
     }
