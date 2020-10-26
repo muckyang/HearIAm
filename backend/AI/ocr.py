e2e37a3132c86d4046a8c90e131fced65b647248
노휘종 <gnlwhd6498@naver.com>
2020-10-26 05:32:04 +0000

    Update backend/AI/ocr.py

--- backend/AI/ocr.py
+++ backend/AI/ocr.py
@@ -62,6 +62,6 @@ def main(argv):
     print(code)
 
 if __name__ == "__main__":
-    sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
-    sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+    # sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+    # sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
     main(sys.argv)
