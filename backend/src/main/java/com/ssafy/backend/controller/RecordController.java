0dc487d006244c784f8ef8a273eaf55a81487d75
김영남 <yn782@naver.com>
2020-10-28 00:16:32 +0900

    Record, RecordAssign Backend Frame Create

--- backend/src/main/java/com/ssafy/backend/controller/RecordController.java
+++ backend/src/main/java/com/ssafy/backend/controller/RecordController.java
@@ -6,13 +6,20 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.time.LocalDateTime;
 
+import com.ssafy.backend.model.Record;
+import com.ssafy.backend.model.RecordAssign;
+import com.ssafy.backend.repository.RecordAssignRepository;
+import com.ssafy.backend.repository.RecordRepository;
+
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.exec.PumpStreamHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,8 +32,15 @@ import io.swagger.annotations.ApiOperation;
 
 @RequestMapping("/api/record")
 public class RecordController {
+
+    @Autowired
+    RecordRepository recordRepository;
+    
+    @Autowired
+    RecordAssignRepository recordAssignRepository;
+    
     @PostMapping("/test")
-    @ApiOperation(value = "녹음 파일 저장")
+    @ApiOperation(value = "녹음파일저장, STT ,WordCloud")
     public Object fileTest(@RequestPart("file") MultipartFile ff) throws IllegalStateException, IOException {
 
         String Rec = ff.getOriginalFilename().toLowerCase();
@@ -57,11 +71,35 @@ public class RecordController {
         
         // return file.getName();
     }
+    @PostMapping("/create")
+    @ApiOperation(value = "녹화상담등록")
+    private Object CreateRecord(@RequestBody Record request){
+        Record record = new Record();
+        record.setTitle(request.getTitle());
+        record.setFilename(request.getFilename());
+        record.setMentee(request.getMentee());
+    
+        recordRepository.save(record);
+        return  0;
+    }
+
+    @PostMapping("/assigned")
+    @ApiOperation(value = "녹화상담 담당하기")
+    private Object AssignedRecord(@RequestBody RecordAssign request){
+
+        RecordAssign recordassign = new RecordAssign();
+        recordassign.setRecordId(request.getRecordId());
+        recordassign.setMento(request.getMento());
+    
+        recordAssignRepository.save(recordassign);
+        return  0;
+    }
+
 
     private long datetimeTosec(LocalDateTime ldt) {
         long result = 0L;
         result += ((((((((ldt.getYear() - 2000) * 365) + ldt.getDayOfYear()) * 24) + ldt.getHour()) * 60)
-                + ldt.getMinute()) * 60);
+                + ldt.getMinute()) * 60+ ldt.getSecond());
         return result;
     }
 
@@ -69,7 +107,6 @@ public class RecordController {
         CommandLine commandLine = CommandLine.parse(command[0]);
         for (int i = 1, n = command.length; i < n; i++) {
             commandLine.addArgument(command[i]);
-            System.out.println("com : "+command[i]);
         }
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         PumpStreamHandler pumpStreamHandler = new PumpStreamHandler(outputStream);
@@ -100,7 +137,5 @@ public class RecordController {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 
-
-        // return outputList;
     }
 }
