125c726454569a96c7e599beb0f6c989e92103cf
임지민 <lmjm1998@naver.com>
2020-11-03 15:48:35 +0900

    add msg alarm logic

--- frontend/src/components/webRTC/UWebRTCComp.vue
+++ frontend/src/components/webRTC/UWebRTCComp.vue
@@ -135,11 +135,18 @@ export default {
           if (res.data > 0) {
             this.dialog = true;
           }
-
-          this.sendDM(res);
+          this.checkCnt(res);
+          // this.sendDM(res);
           this.roomNum = res.data;
         });
       this.onJoin();
+    },
+    checkCnt(res){
+      http.get(`/getMenteeMSGCnt`).then((res)=>{
+        if(res.data == 1){
+          this.sendDM(res);
+        }
+      });
     },
     sendDM(res) {
       const message = {
