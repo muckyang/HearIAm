de24f9ed79c952a3c531c8ecc60d393080d755ec
임지민 <lmjm1998@naver.com>
2020-11-05 05:22:04 +0000

    Update CounselingController.java

--- backend/src/main/java/com/ssafy/backend/controller/CounselingController.java
+++ backend/src/main/java/com/ssafy/backend/controller/CounselingController.java
@@ -287,10 +287,6 @@ public class CounselingController {
 	@GetMapping("/alarmList/{mentorNum}")
 	public List<Alarm> alarmList(@PathVariable(value = "mentorNum") Long mentorNum) {
 		List<Alarm> list = alarmRepository.findAll();
-		System.out.println();
-		for (Alarm alarm : list) {
-			System.out.println(alarm);
-		}
 		return list;
 	}
 
