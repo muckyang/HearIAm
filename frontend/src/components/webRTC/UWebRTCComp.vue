567763068c1ff1c50fb0b91216d0d1ad5941675f
노휘종 <gnlwhd6498@naver.com>
2020-11-11 17:54:45 +0900

    student RTC

--- frontend/src/components/webRTC/UWebRTCComp.vue
+++ frontend/src/components/webRTC/UWebRTCComp.vue
@@ -1,157 +1,50 @@
 <template>
   <div>
-    <v-dialog v-model="dialogRTC" persistent max-width="1000">
-      <v-card height="650" dark>
-        <v-container>
-          <v-stepper v-model="e1">
-            <v-stepper-header>
-              <v-stepper-step :complete="e1 > 1" step="1">
-                실시간 상담 설명 1
-              </v-stepper-step>
-
-              <v-divider></v-divider>
-
-              <v-stepper-step :complete="e1 > 2" step="2">
-                실시간 상담 설명 2
-              </v-stepper-step>
-
-              <v-divider></v-divider>
-
-              <v-stepper-step step="3">
-                실시간 상담 진행
-              </v-stepper-step>
-            </v-stepper-header>
-
-            <v-stepper-items>
-              <v-stepper-content step="1">
-                <v-card class="mb-12 pa-7" align="center" color="grey lighten-1" width="auto" height="400px"
-                  ><h1>HEAR I AM 소개 1</h1>
-                  <div style="width:200px; float:left" >
-                     <v-img
-                     style="float:top; width:200px; max-width: 70%; height: auto;"
-                    src="../../assets/icons/face-detection.png"
-                  ></v-img>
-                <br/>
-                  <v-img
-                    style="float:top; width:200px; max-width: 70%; height: auto;"
-                    src="../../assets/icons/graph.png"
-                  ></v-img>
-                  
-                  </div>
-                  <div style="width:200px; float:left" >
-                    <h4> HEAR I AM 은 상담자에게 얼굴이 공개되지 않는 서비스로 사용자의 익명성이 보장됩니다.</h4>
-                  </div>
-                  </v-card>
-
-                <v-btn color="primary" @click="e1 = 2">
-                  다음
-                </v-btn>
-              </v-stepper-content>
-
-              <v-stepper-content step="2">
-                <v-card class="mb-12 pa-7" color="grey lighten-1" height="400px" >
-                  <v-img
-                    style="float:left;"
-                    width="120px"
-                    height="120px"
-                    src="../../assets/icons/face-detection.png"
-                  ></v-img>
-                  <div float:left>
-                  <h4> HEAR I AM 은 상담자에게 얼굴이 공개되지 않는 서비스로 사용자의 익명성이 보장됩니다.</h4>
-                  </div>
-                  <v-img
-                    style="float:left;"
-                    width="120px"
-                    height="120px"
-                    src="../../assets/icons/graph.png"
-                  ></v-img>
-                  <v-img
-                    style="float:left;"
-                    width="120px"
-                    height="120px"
-                    src="../../assets/icons/anonymity.png"
-                  ></v-img>
-                </v-card>
-                <v-btn color="primary" @click="createRoom">
-                  상담시작!
-                </v-btn>
-              </v-stepper-content>
-
-              <v-stepper-content step="3">
-                <!-- <v-row v-if="!isProgress">
-                  <v-col>
-                    <v-btn
-                      type="button"
-                      class="btn btn-primary"
-                      @click="createRoom"
-                    >
-                      상담 요청
-                    </v-btn>
-                  </v-col>
-                </v-row> -->
-                <v-row v-if="dialog" style="padding-top:200px">
-                  <v-col>
-                    <div>
-                      <h4>상담사와 연결중입니다.</h4>
-                      <br />
-                      <v-progress-circular
-                        :size="100"
-                        :width="7"
-                        color="#49358b"
-                        indeterminate
-                      >
-                        <v-btn text @click="dialogCancel()">취소</v-btn>
-                      </v-progress-circular>
-                    </div>
-                  </v-col>
-                </v-row>
-                <v-row style="padding-top:0px">
-                  <v-col>
-                    <h1 v-if="mentorName">
-                      {{ mentorName }} 상담사와 상담중입니다.
-                    </h1>
-                    <br />
-                    <WebRTC
-                      ref="webrtc"
-                      width="100%"
-                      :roomId="roomId"
-                      cameraHeight="450"
-                      @error="onError"
-                    />
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-spacer></v-spacer>
-                  <v-col>
-                    <v-btn
-                      v-if="endBtn"
-                      type="button"
-                      class="btn btn-primary"
-                      @click="onLeave"
-                    >
-                      상담 종료
-                    </v-btn>
-                  </v-col>
-                </v-row>
-                <!-- <v-btn text>
-                  Cancel
-                </v-btn> -->
-              </v-stepper-content>
-            </v-stepper-items>
-          </v-stepper>
-        </v-container>
-      </v-card>
+    <!-- 설명서 -->
+    <v-dialog v-model="dialogRTC" persistent>
+      <v-alert dark id="step-alert" class="container pa-10">
+        <div class="d-flex mb-7">
+          <v-img max-height="100" max-width="100" src="../../assets/face2.png"></v-img>
+          <h2 class="mt-7 ml-7 step-text">HEAR I AM 은 얼굴이 공개되지 않는 서비스로 사용자의 익명성이 보장됩니다.</h2>
+        </div>
+        <div class="d-flex mb-7">
+          <v-img max-height="100" max-width="100" src="../../assets/face1.png"></v-img>
+          <h2 class="mt-7 ml-7 step-text">카메라는 상담사에게 표정을 보여줄 용도이므로 안심하셔도 됩니다.</h2>
+        </div>
+        <div class="d-flex mb-7">
+          <v-img max-height="100" max-width="100" src="../../assets/face3.png"></v-img>
+          <h2 class="mt-7 ml-7 step-text">버튼을 누르면 상담사가 매칭됩니다.</h2>
+        </div>
+        <div v-if="!dialog">
+          <v-btn class="heartbeat mt-7" color="#262272" @click="dialog = true, dialogRTC2 = true, createRoom()">상담시작</v-btn>
+        </div>
+      </v-alert>
     </v-dialog>
+    
+    <!-- RTC -->
+    <v-overlay :value="dialogRTC2">
+        <div style="padding-top:40px">
+          <v-col>
+            <h1 style="font-size:3vw;color:white;" class="mb-7" v-if="mentorName">{{ mentorName }} 상담사와 상담중입니다.</h1>
+            <WebRTC ref="webrtc" cameraHeight="400" :roomId="roomId" @error="onError"  class="mb-7"/>
+          </v-col>
+        </div>
+        <div>
+          <v-btn v-if="endBtn" type="button" class="btn btn-primary" @click="onLeave">
+            상담 종료
+          </v-btn>
+        </div>
+        <div v-if="dialog">
+          <h2 class="mb-5" style="color:white;">상담사와 연결중입니다.</h2>
+          <v-progress-circular :size="100" :width="5" color="white" indeterminate>
+            <v-btn text @click="dialogCancel()" style="color:white;">취소</v-btn>
+          </v-progress-circular>
+        </div>
+    </v-overlay>
+
     <video id="videoTag" muted hidden @playing="addEventListener()"></video>
 
-    <v-snackbar
-      v-model="successSnack"
-      top
-      flat
-      color="success"
-      rounded="pill"
-      :timeout="2000"
-    >
+    <v-snackbar v-model="successSnack" top flat color="success" rounded="pill" :timeout="2000">
       <span class="snackText">
         {{ altMsg }}
       </span>
@@ -160,15 +53,15 @@
 </template>
 
 <script>
-import WebRTC from "@/components/webRTC/Uwebrtc.vue";
-import * as io from "socket.io-client";
-import * as faceapi from "face-api.js";
+import WebRTC from '@/components/webRTC/Uwebrtc.vue';
+import * as io from 'socket.io-client';
+import * as faceapi from 'face-api.js';
 window.io = io;
-import { mapGetters } from "vuex";
-import http from "@/util/http-common.js";
-import axios from "axios";
+import { mapGetters } from 'vuex';
+import http from '@/util/http-common.js';
+import axios from 'axios';
 export default {
-  name: "UWebRTCComp",
+  name: 'UWebRTCComp',
   components: {
     WebRTC,
   },
@@ -180,40 +73,41 @@ export default {
       message: null,
       videoTag: [],
       isProgress: false,
-      url: "https://fcm.googleapis.com/fcm/send",
+      url: 'https://fcm.googleapis.com/fcm/send',
       devecieId: this.getDeviceID,
-      topic: "streaming",
+      topic: 'streaming',
       dialog: false,
       // failMatching: false,
       roomNum: null,
       dialogRTC: true,
+      dialogRTC2: false,
       endBtn: false,
       mentorName: null,
       successSnack: false,
-      altMsg: "",
+      altMsg: '',
+      startFlag: true,
     };
   },
   mounted() {
     if (this.getRole != `mentee`) {
-      this.$router.push("/");
+      this.$router.push('/');
     }
     this.createRoomId();
     if (this.$route.params.room) {
       this.roomId = this.$route.params.room;
     }
-    this.videoTag = document.getElementById("videoTag");
+    this.videoTag = document.getElementById('videoTag');
     Promise.all([
-      faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-      faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-      faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-      faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+      faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+      faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+      faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+      faceapi.nets.faceExpressionNet.loadFromUri('/models'),
     ]);
   },
   methods: {
     createRoomId(length = 20) {
-      let text = "";
-      const possible =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      let text = '';
+      const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
       Array.from(Array(length)).forEach(() => {
         text += possible.charAt(Math.floor(Math.random() * possible.length));
       });
@@ -221,7 +115,7 @@ export default {
     },
     createRoom() {
       this.e1 = 3;
-      console.log("create Room :::::: " + this.getUserNum);
+      console.log('create Room :::::: ' + this.getUserNum);
       if (this.getUserNum == null) {
         this.$router.go();
       } else {
@@ -230,7 +124,7 @@ export default {
             mentee: this.getUserNum,
             mentor: 1,
             room: this.roomId,
-            status: "liveRequest",
+            status: 'liveRequest',
             date: new Date(),
           })
           .then((res) => {
@@ -238,14 +132,14 @@ export default {
             // this.sendDM(res);
             this.roomNum = res.data;
           });
-        this.dialog = true;
+        // this.dialog = true;
         this.onJoin();
       }
     },
     checkCnt(data) {
-      console.log(" checkCnt ");
+      console.log(' checkCnt ');
       http.get(`/counseling/getMenteeMSGCnt`).then((res) => {
-        console.log(" " + res.data);
+        console.log(' ' + res.data);
         if (res.data == 1) {
           console.log(res.data);
           this.sendDM(data);
@@ -253,34 +147,31 @@ export default {
       });
     },
     sendDM(res) {
-      console.log("sendDM");
+      console.log('sendDM');
       console.dir(res);
       const message = {
         data: {
-          body: "상담을 하고 싶어해요~",
-          title: "학생 클릭 함",
-          icon: "favicon.ico",
+          body: '상담을 하고 싶어해요~',
+          title: '학생 클릭 함',
+          icon: 'favicon.ico',
           room: this.roomId,
           room_num: res.data,
         },
-        to: "/topics/streaming",
+        to: '/topics/streaming',
       };
       const config = {
         headers: {
-          "Content-type": "application/json",
-          Accept: "application/json",
+          'Content-type': 'application/json',
+          Accept: 'application/json',
           Authorization:
-            "key=AAAAEDiSbms:APA91bH-uXikdH1nixzEB2RRH5dMl14_rotnU1ujpcU7Ii6dW-oaV4N_Q6Uh_TvHzumQzllUui2-E4ZdcShX2upbC52FaNAaxxVxjnwnqxcel4RgNYPp_uzWmKNe5OblH2aRX5NWZbcd",
+            'key=AAAAEDiSbms:APA91bH-uXikdH1nixzEB2RRH5dMl14_rotnU1ujpcU7Ii6dW-oaV4N_Q6Uh_TvHzumQzllUui2-E4ZdcShX2upbC52FaNAaxxVxjnwnqxcel4RgNYPp_uzWmKNe5OblH2aRX5NWZbcd',
         },
       };
       axios
         .post(this.url, message, config)
         .then((response) => {
           if (response.status < 200 || response.status >= 400) {
-            throw "Error subscribing to topic: " +
-              response.status +
-              " - " +
-              response.text();
+            throw 'Error subscribing to topic: ' + response.status + ' - ' + response.text();
           }
         })
         .catch((e) => {
@@ -289,18 +180,21 @@ export default {
     },
     onJoin() {
       this.isProgress = true;
-      this.$refs.webrtc.join();
+      this.dialogRTC = false;
+      setTimeout(() => {
+        this.$refs.webrtc.join();
+      }, 1000);
       this.startVideo();
     },
     onLeave() {
-      this.$store.commit("changeIsRemote", false);
+      this.$store.commit('changeIsRemote', false);
       this.$refs.webrtc.leave();
       this.stopVideo();
       this.deleteDB();
-      this.$router.push("/");
+      this.$router.push('/');
     },
     onError(error, stream) {
-      console.log("On Error Event", error, stream);
+      console.log('On Error Event', error, stream);
     },
     sendMessage(data) {
       this.$refs.webrtc.sendMessage(data);
@@ -325,13 +219,13 @@ export default {
     },
     startVideo() {
       navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-        const videoPlayer = document.getElementById("videoTag");
+        const videoPlayer = document.getElementById('videoTag');
         videoPlayer.srcObject = stream;
         videoPlayer.play();
       });
     },
     stopVideo() {
-      if (this.videoTag.srcObject != null && this.videoTag.srcObject != "") {
+      if (this.videoTag.srcObject != null && this.videoTag.srcObject != '') {
         const tracks = this.videoTag.srcObject.getTracks();
         tracks.forEach(function(track) {
           track.stop();
@@ -350,30 +244,28 @@ export default {
       this.onLeave();
     },
     deleteDB() {
-      http
-        .delete(`/counseling/deleteReadyMentee/${this.roomNum}`)
-        .then(() => {});
+      http.delete(`/counseling/deleteReadyMentee/${this.roomNum}`).then(() => {});
     },
   },
   computed: {
     ...mapGetters([
-      "isProfileLoaded",
-      "getRole",
-      "getQualification",
-      "getUserName",
-      "getUserNum",
-      "getUserID",
-      "getIsRemote",
-      "getDeviceID",
+      'isProfileLoaded',
+      'getRole',
+      'getQualification',
+      'getUserName',
+      'getUserNum',
+      'getUserID',
+      'getIsRemote',
+      'getDeviceID',
     ]),
   },
   watch: {
     getIsRemote(val) {
-      console.log("isremote + " + val);
+      console.log('isremote + ' + val);
       if (val) {
-        console.log("remote 들어옴");
+        console.log('remote 들어옴');
         this.successSnack = true;
-        this.altMsg = "상담가가 들어옵니다.";
+        this.altMsg = '상담가가 들어옵니다.';
         this.dialog = false;
         this.endBtn = true;
         http.get(`/counseling/liveMentorInfo/${this.roomId}`).then((res) => {
@@ -386,7 +278,7 @@ export default {
       if (!val) {
         return;
       }
-      console.log("isremote " + this.getIsRemote);
+      console.log('isremote ' + this.getIsRemote);
       // setTimeout(() => {
       //   this.dialog = false;
       //   // this.failMatching = true;
@@ -397,3 +289,86 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+#step-alert {
+  margin: 0 auto;
+  background-color: #ff7987;
+  height: 550px;
+}
+.step-text {
+  font-size: 2vw;
+}
+.heartbeat {
+  -webkit-animation: heartbeat 2s ease-in-out infinite both;
+  animation: heartbeat 2s ease-in-out infinite both;
+}
+@-webkit-keyframes heartbeat {
+  from {
+    -webkit-transform: scale(1);
+    transform: scale(1);
+    -webkit-transform-origin: center center;
+    transform-origin: center center;
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+  }
+  10% {
+    -webkit-transform: scale(0.91);
+    transform: scale(0.91);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+  17% {
+    -webkit-transform: scale(0.98);
+    transform: scale(0.98);
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+  }
+  33% {
+    -webkit-transform: scale(0.87);
+    transform: scale(0.87);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+  45% {
+    -webkit-transform: scale(1);
+    transform: scale(1);
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+  }
+}
+@keyframes heartbeat {
+  from {
+    -webkit-transform: scale(1);
+    transform: scale(1);
+    -webkit-transform-origin: center center;
+    transform-origin: center center;
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+  }
+  10% {
+    -webkit-transform: scale(0.91);
+    transform: scale(0.91);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+  17% {
+    -webkit-transform: scale(0.98);
+    transform: scale(0.98);
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+  }
+  33% {
+    -webkit-transform: scale(0.87);
+    transform: scale(0.87);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+  45% {
+    -webkit-transform: scale(1);
+    transform: scale(1);
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+  }
+}
+</style>
--- frontend/src/views/webRTC/UserWRTC.vue
+++ frontend/src/views/webRTC/UserWRTC.vue
@@ -1,25 +1,29 @@
 <template>
   <div
-      style="
+    id="student-con"
+    style="
         height: 100vh;
         width: 100%;
-        background: linear-gradient(to right, #93dfff, #f5a2bb);
         padding-top: 64px;
       "
-    >
+  >
     <WebRTCComp />
   </div>
 </template>
 
 <script>
-import WebRTCComp from "@/components/webRTC/UWebRTCComp.vue";
+import WebRTCComp from '@/components/webRTC/UWebRTCComp.vue';
 export default {
-  name: "UserWRTC",
+  name: 'UserWRTC',
   components: {
     WebRTCComp,
   },
 };
 </script>
 
-<style>
+<style scoped>
+#student-con {
+  background-image: url('../../assets/recordMain.png');
+  background-size: cover;
+}
 </style>
